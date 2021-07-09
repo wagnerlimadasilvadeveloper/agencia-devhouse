81d6edb416314f5d57d3041bdf1495c3c2787cd8
Wagner Lima <wagnerlima.coach@gmail.com>
2021-07-08 23:35:13 -0300

    Incluindo blog e rotas

--- app.js
+++ app.js
@@ -2,15 +2,28 @@ const express =require ('express');
 let app = express();
 app.listen(3030,()=>{
 
+    app.use(express.static('public'));
+
     app.get('/',(req,res)=>{
-        res.send('Seja bem vindo');
+        res.sendFile(__dirname + "/views/home.html");
+    
+    });
+ 
+
+    app.get('/home',(req,res)=>{
+        res.sendFile(__dirname + "/views/home.html");
     
     });
 
     app.get('/manutencao',(req,res)=>{
         res.sendFile(__dirname + "/views/manutencao.html");
     
-    })
+    });
+
+    app.get('/blog',(req,res)=>{
+        res.sendFile(__dirname + "/views/blog.html");
+    
+    });
  
     console.log("servidor rodando");
 });
